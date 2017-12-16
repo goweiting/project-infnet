840d3fa2779a1a7fc9a5e617ee7df773ceb79910
goweiting <gwt.543@gmail.com>
2017-12-16 01:04:29 +0000

    integrating infnet-analysis and scrapper

---
+++ infnet-analysis/notebooks/helper.py
@@ -0,0 +1,116 @@
+"""
+A collection of useful functions!
+"""
+
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+from multiprocessing import Pool
+import itertools
+import powerlaw
+
+
+def avg_degree_dist(degree_seq):
+    n = np.sum(degree_seq)
+    expected = 0
+    for i, d in enumerate(degree_seq):
+        expected += float(i) * float(d) / n
+    # print(expected)
+    return expected
+
+def power_law_fit(degree_seq):
+
+    fit = powerlaw.Fit(degree_seq, discrete=True, xmin=0.) # Fit the data
+    fig = fit.plot_ccdf(color='b', linewidth=2, label='Empirical ccdf') # plot the data
+    fit.power_law.plot_ccdf(linestyle='--', color='b', linewidth=2, label='Power Law Fit', ax=fig) #plot the data
+    alpha = fit.power_law.alpha
+    
+    fig.set_ylabel(r'$F(d) =P(D_{v} \geq d)$')
+    fig.set_xlabel('d')
+    fig.legend(loc='best')
+    #     fig.set_title(r'Power law fit: $F(d) = (\frac{d}{d_{min}})^{-(\alpha-1)}$')
+    fig.set_title(r'Power law fit ($\alpha=${:.2f})'.format(alpha))
+    return fig
+
+def degree_dist(G, show=True):
+    """
+    given a graph G, generate a degree histogram:
+    """
+    degree_seq = nx.degree_histogram(G)
+
+    fig = plt.figure(figsize=(7, 7))
+    ax = fig.add_subplot(111)
+    # n = len(G)
+    # k = G.size()
+    ax.hist(degree_seq)
+    ax.set_xlabel('degree')
+    ax.set_ylabel('count')
+    ax.set_title('Average degree = {}'.format(avg_degree_dist(degree_seq)))
+    plt.show()
+    return ax, degree_seq
+
+
+def generateGCC(G):
+    """
+    returns a list of connected components, sorted in reverse order - from largest to smallest
+    Also print the size of each component wrt to the graph G
+    """
+    total_nodes = len(G)
+    gccs = sorted(nx.connected_component_subgraphs(G), key=len, reverse=True)
+    percentage = []
+    for i, g in enumerate(gccs, 1):
+        percent = float(len(g)) / total_nodes
+        percentage.append(percent)
+        print('component {}: {:.2%}'.format(i, percent))
+    return gccs, percentage
+
+def clustering_coeff(G):
+    c_coeff = nx.clustering(G)
+    avg_c = np.mean(c_coeff.values())
+    return c_coeff, avg_c
+
+
+def centrality_measure(G):
+    nodes_centrallity = nx.degree_centrality(G)
+
+    
+def partitions(nodes, n):
+    "Partitions the nodes into n subsets"
+    nodes_iter = iter(nodes)
+    while True:
+        partition = tuple(itertools.islice(nodes_iter, n))
+        if not partition:
+            return
+        yield partition
+
+def btwn_pool(G_tuple):
+    return nx.betweenness_centrality_source(*G_tuple)
+
+
+# To begin the parallel computation, we initialize a Pool object with the
+# number of available processors on our hardware. We then partition the
+# network based on the size of the Pool object (the size is equal to the
+# number of available processors).
+def between_parallel(G, processes=None):
+    p = Pool(processes=processes)
+    part_generator = 4 * len(p._pool)
+    node_partitions = list(partitions(G.nodes(), int(len(G) / part_generator)))
+    num_partitions = len(node_partitions)
+
+    #Next, we pass each processor a copy of the entire network and
+    #compute #the betweenness centrality for each vertex assigned to the
+    #processor.
+
+    bet_map = p.map(btwn_pool,
+                    zip([G] * num_partitions, [True] * num_partitions,
+                        [None] * num_partitions, node_partitions))
+
+    #Finally, we collect the betweenness centrality calculations from each
+    #pool and aggregate them together to compute the overall betweenness
+    #centrality score for each vertex in the network.
+
+    bt_c = bet_map[0]
+    for bt in bet_map[1:]:
+        for n in bt:
+            bt_c[n] += bt[n]
+    return bt_c
---
+++ infnet-scrapper/scrapper/spiders/peopleSpider.py
@@ -0,0 +1,117 @@
+import scrapy
+import re
+from scrapy.crawler import CrawlerProcess
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst, MapCompose, Join
+
+# Global function to remove items except for alphanumeric
+rmUnwantedChars = lambda x: (re.sub(r'[^ a-zA-Z]', '', x)).strip().lower()
+
+
+class Person(scrapy.Item):
+    id = scrapy.Field()
+    personal_url = scrapy.Field()
+    organisation = scrapy.Field()
+    first_name = scrapy.Field(
+        input_processor=MapCompose(lambda x: x.strip().lower()),
+        output_processor=TakeFirst()
+    )
+    last_name = scrapy.Field(
+        input_processor=MapCompose(lambda x: x.strip().lower()),
+        output_processor=TakeFirst()
+    )
+
+
+class PersonLoader(ItemLoader):
+    default_item_class = Person
+    default_output_processor = TakeFirst()
+
+
+class ResearchExplorerSpider(scrapy.Spider):
+    name = "peopleSpider"
+    custom_settings = {
+        'ITEM_PIPELINES': {
+            'scrapper.pipelines.PersonPipeline': 300
+        }
+    }
+
+    urls = [
+        """http://www.research.ed.ac.uk/portal/en/organisations/school-of-informatics(d9a3581f-93a4-4d74-bf29-14c86a1da9f4)/persons.html"""]
+
+    def start_requests(self):
+        try:
+            for url in self.urls:
+                yield scrapy.Request(url=url, callback=self.parsePerson)
+        except:
+            pass
+
+    def parsePerson(self, response):
+        """ Extract the particulars of individual from School of Infromatics
+        Each item in the portal_list_item is a information about the the individual.
+        The following information are scrapped:
+        [+] the url of the individual
+        [+] the unique id that the research explorer uses
+        [+] first name, last name
+        [+] the organisation that individual belongs to and his/her role in it
+        """
+        for sel in response.xpath('//li[@class="portal_list_item"]'):
+            persLoader = PersonLoader(selector=sel)
+            persLoader.add_xpath('personal_url', './/a[@class="link person"]/@href')
+            persLoader.add_value('id',
+                                 re.findall(r'\((.*)\)', persLoader.get_output_value('personal_url'))[0])
+            full_name = sel.xpath('.//a[@class="link person"]/span/text()').extract_first()
+            last_name, first_name = full_name.split(',')
+            persLoader.add_value('last_name', last_name)
+            persLoader.add_value('first_name', first_name)
+
+            # Scrap all the link_organisation that an individual belongs to:
+            orgs_info = {'parent': None, 'institute': None, 'position': None}
+            orgs_info['position'] = \
+                rmUnwantedChars(sel.xpath('.//span[@class="minor dimmed"]/text()').extract_first(default='UNKNOWN'))
+
+            org = sel.xpath('.//a[@class="link organisation"]')
+            parent_org = org[0]
+
+            orgs_info['parent'] = \
+                rmUnwantedChars(parent_org.xpath('.//span/text()').extract_first(default='UNKNOWN'))
+
+            try:
+                institute = org[1]
+                orgs_info['institute'] = \
+                    rmUnwantedChars(institute.xpath('.//span/text()').extract_first())
+            except IndexError or TypeError:
+                orgs_info['institute'] = 'UNKNOWN'
+
+            persLoader.add_value('organisation', orgs_info)
+
+            # Return the individual's information
+            yield persLoader.load_item()
+
+            # Basically clicks the `next` button
+            for sel in response.xpath('//a[@class="portal_navigator_next common_link"]'):
+                next = sel.xpath('@href').extract_first()  # extract the link to the next page
+                # print('next:', next)
+                yield scrapy.Request(url=next, callback=self.parsePerson)
+
+
+
+# =====================================================================================
+def main():
+    depth = 100
+    delay = 0.2
+
+    process = CrawlerProcess({
+        #  'USER_AGENT': 'Chrome/58.0',
+        'DEPTH_LIMIT': depth,
+        'DOWNLOAD_DELAY': delay,
+        'CONCURRENT_REQUESTS': 1000
+    })
+
+    spider = ResearchExplorerSpider()
+    process.crawl(spider)
+    process.start()  # the script will block here until the crawling is finished
+    # spider.finalize()
+
+
+if __name__ == "__main__":
+    main()
