01f9830c6d61533a363fa2fa0b3d6d1c0dcaad7c
s1450710@sms.ed.ac.uk <s1450710@sms.ed.ac.uk>
2018-01-29 16:31:16 +0000

    ran finding topics with ldatuning

---
+++ topicModel/src/model1/ldatuning.r
@@ -0,0 +1,39 @@
+#!/usr/bin/Rscript
+
+# Packages:
+library('tm')
+library('ldatuning')
+library('topicmodels')
+#library('magrittr')
+
+# Loading documents
+docs <- scan(file = '../../../data/data_schoolofinf/toks/toks2word.all', character(), sep = '\n')
+# Create corpus
+corpus <- Corpus(VectorSource(docs))
+# Create a document-term matrix for LDA
+dtm <- DocumentTermMatrix(corpus)
+
+# Metrics for LDA
+control_list_gibbs <- list(
+  burnin = 2500,
+  iter = 5000,
+  seed = 0:4,
+  nstart = 5,
+  best = TRUE
+)
+
+
+# Executing larger number of topics: NOTE: TOPIC_NUMBER2
+system.time(
+  topic_number1 <- FindTopicsNumber(
+    dtm,
+    topics = c(seq(5,10,1), seq(12, 20, 2), seq(25, 50, 5), seq(60,100, 10)),
+    metrics = c( "Griffiths2004", "CaoJuan2009", "Arun2010", "Deveaud2014"),
+    method = "Gibbs",
+    control = control_list_gibbs,
+    mc.cores = 40L,
+    verbose = TRUE
+  )
+)
+save(list=c("topic_number1"), file="./tuning.topic_number.rdata")
+print("===COMPLETED EXECUTION===")
