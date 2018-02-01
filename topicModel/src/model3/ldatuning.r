b0ef802db40e3762ea9c471616e6d515bd92f81c
s1450710@sms.ed.ac.uk <s1450710@sms.ed.ac.uk>
2018-02-01 00:51:08 +0000

    using numbers instead of words; error in previous toks generated

--- topicModel/src/model3/ldatuning.r
+++ topicModel/src/model3/ldatuning.r
@@ -7,11 +7,14 @@ library('topicmodels')
 #library('magrittr')
 
 # Loading documents
-docs <- scan(file = '../../../data/data_schoolofinf/toks/toks2word.less.all', character(), sep = '\n')
+docs <- scan(file = '../../../data/data_schoolofinf/toks/bow2idx.less.all', character(), sep = '\n')
 # Create corpus
 corpus <- Corpus(VectorSource(docs))
 # Create a document-term matrix for LDA
 dtm <- DocumentTermMatrix(corpus)
+inspect(dtm)
+rowTotals <- apply(dtm , 1, sum)
+dtm.new   <- dtm[rowTotals> 0, ]
 
 # Metrics for LDA
 control_list_gibbs <- list(
@@ -26,12 +29,12 @@ control_list_gibbs <- list(
 # Executing larger number of topics: NOTE: TOPIC_NUMBER2
 system.time(
   topic_number1 <- FindTopicsNumber(
-    dtm,
+    dtm.new,
     topics = c(seq(5,10,1), seq(12, 20, 2), seq(25, 50, 5), seq(60,100, 10)),
     metrics = c( "Griffiths2004", "CaoJuan2009", "Arun2010", "Deveaud2014"),
     method = "Gibbs",
     control = control_list_gibbs,
-    mc.cores = 40L,
+    mc.cores = 10L,
     verbose = TRUE
   )
 )
