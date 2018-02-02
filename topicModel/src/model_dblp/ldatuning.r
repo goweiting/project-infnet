84f6b7fdcdc0088cfd3f843da2e6b0d9ed350458
s1450710@sms.ed.ac.uk <s1450710@sms.ed.ac.uk>
2018-02-02 11:06:40 +0000

    results for ldatuning using the corect BOW representation

---
+++ topicModel/src/model_dblp/ldatuning.r
@@ -0,0 +1,42 @@
+#!/usr/bin/Rscript
+
+# Packages:
+library('tm')
+library('ldatuning')
+library('topicmodels')
+#library('magrittr')
+
+# Loading documents
+docs <- scan(file = '../../../data/data_dblp/toks/bow2idx.all', character(), sep = '\n')
+# Create corpus
+corpus <- Corpus(VectorSource(docs))
+# Create a document-term matrix for LDA
+dtm <- DocumentTermMatrix(corpus)
+inspect(dtm)
+rowTotals <- apply(dtm , 1, sum)
+dtm.new   <- dtm[rowTotals> 0, ] # REMOVE EMPTY DOCS
+
+# Metrics for LDA
+control_list_gibbs <- list(
+  burnin = 2500,
+  iter = 5000,
+  seed = 0:4,
+  nstart = 5,
+  best = TRUE
+)
+
+
+# Executing larger number of topics: NOTE: TOPIC_NUMBER2
+system.time(
+  topic_number1 <- FindTopicsNumber(
+    dtm.new,
+    topics = seq(12, 20, 2), seq(25, 50, 5), seq(60,100, 10)),
+    metrics = c( "Griffiths2004", "CaoJuan2009", "Arun2010", "Deveaud2014"),
+    method = "Gibbs",
+    control = control_list_gibbs,
+    mc.cores = 4L,
+    verbose = TRUE
+  )
+)
+save(list=c("topic_number1"), file="./tuning.topic_number.rdata")
+print("===COMPLETED EXECUTION===")
